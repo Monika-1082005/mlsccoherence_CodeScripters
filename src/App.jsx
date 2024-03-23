2b5f9d8de976f686d25036784f873bd38760095f
Monika Dalawat <134940879+Monika-1082005@users.noreply.github.com>
2024-03-23 19:11:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-// import Card from './components/Card';
-// import Hashtag from './components/Hashtag';
+import HomeContent from './components/HomeContent';
 import './App.css'
-// import { AppSidebarNav } from './components/AppSidebarNavbar';
 
 function App() {
-
-
   return (
     <Router>
-    <div className="container">
-      <Navbar />
-      {/* <Card /> */}
-      {/* <AppSidebarNavbar /> */}
-      <Routes>
-        <Route exact path="/Login" element={<Login />} />
-        <Route exact path="/SignUp" element={<SignUp />} />
-      </Routes>
-      {/* <Hashtag /> */}
-    </div>
-  </Router>
-  )
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route exact path="/Login" element={<Login />} />
+          <Route exact path="/SignUp" element={<SignUp />} />
+          <Route exact path="/HomeContent" element={<HomeContent />} /> {/* Add this Route */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
