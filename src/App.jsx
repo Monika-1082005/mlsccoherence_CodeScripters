f81714e2bafc36fa020ad2bc566ed9afd8d637a1
Monika-1082005 <dalawatmonika2801@gmail.com>
2024-03-24 09:39:54 +0530

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import HomeContent from './components/HomeContent';
-import './App.css'
+// import GeminiChatbot from './components/GeminiChatbot';
+// import SocialMediaIcons from './components/SocialMediaIcons';
+// import TooltipContainer from './components/TooltipContainer';
+import './App.css';
+
 
 function App() {
+  // const [images, setImages] = useState([]);
+  // const accessKey = process.env.REACT_APP_ACCESS_KEY;
+
+  // useEffect(() => {
+  //   // Fetch images when the component mounts
+  //   fetch(`https://api.unsplash.com/photos/random?client_id=${accessKey}&count=10`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setImages(data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching images:', error);
+  //     });
+  // }, [accessKey]);
+
   return (
     <Router>
       <div className="container">
+        
         <Navbar />
+        <HomeContent />
         <Routes>
           <Route exact path="/Login" element={<Login />} />
           <Route exact path="/SignUp" element={<SignUp />} />
-          <Route exact path="/HomeContent" element={<HomeContent />} /> {/* Add this Route */}
         </Routes>
+        {/* <SocialMediaIcons /> */}
+        {/* <GeminiChatbot /> */}
+        <Footer />
       </div>
     </Router>
   );
